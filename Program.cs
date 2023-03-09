471a90c38b6d7fb42e4abefe63ae039024e218dc
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-09 19:28:58 +0530

    Greeting message for Employee Wage problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Employeewagecomputationproblem
+{
+
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation problem");
+            Employeecheck.EmployeeAttendence();
+
+        }
+    }
+}
